fcc3b791bcf5639e3800f76e8f4cf68850f2a26c
thun888 <63234268+thun888@users.noreply.github.com>
2022-01-01 16:42:32 +0800

    Add files via upload

---
+++ files/otna.js
@@ -0,0 +1,17 @@
+//Otn Analytics
+//version 0.0.1
+var otna_domain = window.location.host;
+var otna_pathname = window.location.pathname;
+var url = "https://tongji.thun888.xyz:456/index.php";
+fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ domain: otna_domain, pathname: otna_pathname })
+  }).then(function(response) {
+  return response.json();
+})
+.then(function(myJson) {
+    otna_res = myJson;
+    document.getElementById("otna_value_page_pv").innerHTML=otna_res.pagepv;
+    document.getElementById("otna_value_site_pv").innerHTML=otna_res.allpv;
+  })
